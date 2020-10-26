b3c101924f23aeb3c0b8085c5f8039b834decb7f
Kyle Grant <kyle.grant5@gmail.com>
2020-10-26 20:28:46 +0000

    Fixed registration bug

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -24,5 +24,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
        unless(params[:plan] == '1' || params[:plan] == '2')
            flash[:notice] = "Please select a membership plan to sign up."
            redirect_to root_url
+        end
     end
 end
