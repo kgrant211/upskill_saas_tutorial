4b125b37cdb7bbff1070cf6422db736d99963a71
Kyle Grant <kyle.grant5@gmail.com>
2020-10-27 18:37:10 +0000

    Udpated profile page to show description styles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -14,7 +14,7 @@ class ProfilesController < ApplicationController
       @profile = @user.build_profile( profile_params )
       if @profile.save
           flash[:success] = "Profile saved!"
-          redirect_to user_path( params [:user_id] )
+          redirect_to user_path( params[:user_id] )
       else
           render action: :new
       end
@@ -23,5 +23,5 @@ class ProfilesController < ApplicationController
    private
     def profile_params
     params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
-   end
+    end
 end
