fad47146525cdecf659733782b37854fb430f1de
Kyle Grant <kyle.grant5@gmail.com>
2020-10-27 23:57:22 +0000

    Added before filters to check correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+   before_action :authenticate_user!
+   before_action :only_current_user
    
    #GET to /users/:user_id/profile/new
    def new
@@ -22,13 +24,33 @@ class ProfilesController < ApplicationController
    
    #GET to /users/:user_id/profile/edit
    def edit
-       @user = User.find(params[:user_id])
+       @user = User.find( params[:user_id] )
        @profile = @user.profile
-       
+   end
+   
+   #PUT to /users/:user_id/profile
+   def update
+       # Retrieve user from the database
+       @user = User.find( params[:user_id] )
+       # Retrieve that user's profile
+       @profile = @user.profile
+       # Mass assign edited profile attributes and save (update)
+       if @profile.update_attributes(profile_params)
+           flash[:success] = "Profile updated!"
+           # Redirect user to profile page
+           redirect_to user_path(id: params[:user_id])
+       else
+           render action: :edit
+       end
    end
    
    private
     def profile_params
     params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+    end
+    
+    def only_current_user
+       @user = User.find( params[:user_id] )
+       redirect_to(root_url) unless @user == current_user
     end
 end
