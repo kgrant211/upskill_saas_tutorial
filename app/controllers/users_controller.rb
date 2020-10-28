a508963a3cd02070456d7d37568368cae4270300
Kyle Grant <kyle.grant5@gmail.com>
2020-10-28 13:48:18 +0000

    Styled the community page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
    
    def index
-       
+      @users = User.includes(:profile)
    end
    
    # GET request made to /users/:id
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,11 @@
 module UsersHelper
    def job_title_icon
       if @user.profile.job_title == "Developer"
-          "<i class='fas fa-code'></i>".html_safe
+          "<i class='fas fa-code fa-fw'></i>".html_safe
       elsif @user.profile.job_title == "Entrepreneur"
-          "<i class='far fa-lightbulb'></i>".html_safe
+          "<i class='far fa-lightbulb fa-fw'></i>".html_safe
       elsif @user.profile.job_title == "Investor"
-          "<i class='fas fa-dollar-sign'</i>".html_safe
+          "<i class='fas fa-dollar-sign fa-fw'</i>".html_safe
       end
    end
 end
