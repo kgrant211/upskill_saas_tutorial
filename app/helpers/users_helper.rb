da9efd589dbaabc856326fe2be4dec54cdc66d58
Kyle Grant <kyle.grant5@gmail.com>
2020-11-06 04:51:22 +0000

    Updated home

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,10 @@
 module UsersHelper
    def job_title_icon
-      if @user.profile.job_title == "Developer"
+      if @user.profile.services_offered == "Developer"
           "<i class='fas fa-code fa-fw'></i>".html_safe
-      elsif @user.profile.job_title == "Entrepreneur"
+      elsif @user.profile.services_offered == "Entrepreneur"
           "<i class='far fa-lightbulb fa-fw'></i>".html_safe
-      elsif @user.profile.job_title == "Investor"
+      elsif @user.profile.services_offered == "Investor"
           "<i class='fas fa-dollar-sign fa-fw'</i>".html_safe
       end
    end
